2c4521797e5a5913a4aa8fab227d222182e17b82
Nehan Ahmed <nehanahmed2k23@gmail.com>
2026-05-05 13:31:28 +0500

    Added the signup and signin functionality in the frontend.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 const authRoutes = require('./routes/auth.routes')
 const musicRoutes = require('./routes/music.routes')
 const app = express()
 app.use(express.json()) 
+app.use(cors({methods:["GET","POST","PUT","DELETE"],
+
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
 app.use(cookieParser())
 app.use('/api/auth',authRoutes)
 app.use('/api/music',musicRoutes)
